9bdd8f031661676b87ab00fad22b261a885ad0d4
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-09-15 18:16:03 +0600

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -8,18 +8,18 @@ class Vehicle:
         self.__color = __color
 
     def get_model(self):
-        print(f"Модель: {self.__model}")
+        return f"Модель: {self.__model}"
 
     def get_horsepower(self):
-        print(f"Мощность двигателя: {self.__engine_power}")
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
-        print(f"Цвет: {self.__color}")
+        return f"Цвет: {self.__color}"
 
     def print_info(self):
-        self.get_model()
-        self.get_horsepower()
-        self.get_color()
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f"Владелец: {self.owner}")
 
     def set_color(self, new_color):
